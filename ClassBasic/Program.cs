8bd71fd37fd9dd30409815aa2ac7b4662dbb19b7
Chaeyoung Lim <rnldyal1027@naver.com>
2023-05-08 12:17:10 +0900

    class이름이 겹칠 때

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace NewClassProgram
+{
+    internal class Program
+    {
+        private class Math {
+        static public int Abs(int a){
+                return 0;
+            }
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine(Math.Abs(-10));
+
+            ThirdClass abc;
+            ABCClass abc2;
+            InnerClass inn;
+            Console.WriteLine("Hello World!");
+        }
+
+        private class InnerClass
+        {
+        }
+    }
+
+    internal class ABCClass
+    {
+    }
+}
+
